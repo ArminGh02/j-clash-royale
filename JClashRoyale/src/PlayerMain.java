7a544b8b472592202e77f183484309a946455626
Amirhesam Adibinia <hesamadibi80@gmail.com>
2021-06-28 17:36:07 +0430

    Make project files

---
+++ JClashRoyale/src/PlayerMain.java
@@ -0,0 +1,14 @@
+/**
+ * PlayerMain class, start a new client and connect him to the server
+ * @version 1.0
+ * @author Adibov & ArminGh
+ */
+public class PlayerMain {
+    /**
+     * main method, makes class runnable
+     * @param args program args
+     */
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ JClashRoyale/src/ServerMain.java
@@ -0,0 +1,14 @@
+/**
+ * ServerMain class, runs server and make ready to accept players
+ * @version 1.0
+ * @author Adibov & Armin Gh
+ */
+public class ServerMain {
+    /**
+     * main method, makes class runnable
+     * @param args program args
+     */
+    public static void main(String[] args) {
+
+    }
+}
