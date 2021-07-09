10e73fd6bc739eb1f7d0642c8aa19d05dfdccfa9
Amirhesam Adibinia <hesamadibi80@gmail.com>
2021-07-09 14:49:40 +0430

    Add Person class

---
+++ JClashRoyale/src/model/Password.java
@@ -0,0 +1,56 @@
+package model;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Password class, handles actions on password like hashing, parsing, etc
+ * @author Adibov & Armin Gh
+ * @version 1.0
+ */
+public class Password {
+    String hashedPassword;
+
+    /**
+     * class constructor, hash the given password and save it
+     * @param password given password
+     */
+    public Password(String password) {
+    }
+
+    /**
+     * hash the given password using sha256 algorithm
+     * @param password given password
+     * @return hashed password
+     */
+    public static String hashPassword(String password) {
+        MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedHash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            return Password.bytesToHex(encodedHash);
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * convert given byte array to string
+     * @param hash given byte array
+     * @return result string
+     */
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+}
---
+++ JClashRoyale/src/model/Player.java
@@ -0,0 +1,10 @@
+package model;
+
+/**
+ * player class, handles information/action for players
+ * @author Adibov & Armin Gh
+ * @version 1.0
+ */
+public abstract class Player {
+    private String displayName;
+}
