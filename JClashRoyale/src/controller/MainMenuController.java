69fef11cdd4278038ba68c1b0bc7cf92577f6a94
Amirhesam Adibinia <hesamadibi80@gmail.com>
2021-07-11 12:11:32 +0430

    Add logout button and new tabs

--- JClashRoyale/src/controller/MainMenuController.java
+++ JClashRoyale/src/controller/MainMenuController.java
@@ -1,5 +1,21 @@
 package controller;
 
-public class MainMenuController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import view.ViewManager;
 
+/**
+ * MainMenuController class, handles interactions in main menu
+ * @author Adibov & Armin Gh
+ * @version 1.0
+ */
+public class MainMenuController {
+    /**
+     * logout from the current account and return to log in view
+     * @param event occurred event
+     */
+    @FXML
+    void logoutPressed(ActionEvent event) {
+        ViewManager.loadLoginView();
+    }
 }
